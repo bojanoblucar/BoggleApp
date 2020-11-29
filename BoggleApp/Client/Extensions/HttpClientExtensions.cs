289d3a19b534968ef3f96bc3ae91538c5d0ca2d1
bojanoblucar <bojan.oblucar@gmail.com>
2020-11-29 01:54:14 +0100

    adding extensions

---
+++ BoggleApp/Client/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BoggleApp.Shared.Helpers;
+
+namespace BoggleApp.Client.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<Result<T>> PostAsJsonWithResultAsync<T, TContent>(this HttpClient httpClient, string path, TContent content)
+        {
+            var result = await httpClient.PostAsJsonAsync(path, content);
+
+            return await GetResultFromHttpResponseMessage<T>(result);
+        }
+
+
+        public static async Task<Result<T>> GetAsResult<T>(this HttpClient httpClient, string path)
+        {
+            var result = await httpClient.GetAsync(path);
+
+            return await GetResultFromHttpResponseMessage<T>(result);
+        }
+
+        private static async Task<Result<T>> GetResultFromHttpResponseMessage<T>(HttpResponseMessage response)
+        {
+            var isValid = response.IsSuccessStatusCode;
+            var value = default(T);
+            if (response.IsSuccessStatusCode)
+            {
+                value = await response.Content.ReadFromJsonAsync<T>();
+            }
+
+            return new Result<T>
+            {
+                Value = value,
+                IsValid = isValid
+            };
+        }
+    }
+
+}
---
+++ BoggleApp/Server/Helpers/GameTicker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using BoggleApp.Server.Hubs;
+using BoggleApp.Shared;
+using BoggleApp.Shared.Enums;
+using BoggleApp.Shared.Repositories;
+using BoggleApp.Shared.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BoggleApp.Server.Helpers
+{
+    public class GameTicker : IGameTicker
+    {
+        IHubContext<GameHub> _context;
+
+        private bool _stopCounting = false;
+
+        public GameTicker(IHubContext<GameHub> context)
+        {
+            _context = context;
+        }
+
+        public async void StartCountdown(Room room, int countdownInSeconds)
+        {
+            _stopCounting = false;
+            if (room.GameStatus == RoomStatus.PlayMode)
+            {
+                //await Task.Delay(500);
+                int remained = countdownInSeconds;
+                while (remained >= 0 && !_stopCounting)
+                {
+                    await _context.Clients.Group(room.Id).SendAsync("TimeLeft", remained.ToString());
+                    await Task.Delay(1000);
+                    
+                    remained -= 1;
+                }
+
+                
+                room.GameStatus = RoomStatus.Initialized;
+                //await _context.Clients.Group(room.Id).SendAsync("GameOver", remained.ToString());
+            }
+        }
+
+        public void StopCountdown()
+        {
+            _stopCounting = true;
+        }
+    }
+}
